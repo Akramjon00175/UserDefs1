b4d97faf80e97770f6c814563aec6fdd45c0ff36
Akramjon <akramjon0175@gmail.com>
2023-09-29 08:19:06 -0700

    Initial Commit

---
+++ UserDefs/UserDefsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserDefsApp.swift
+//  UserDefs
+//
+//  Created by Akramjon on 29/09/23.
+//
+
+import SwiftUI
+
+@main
+struct UserDefsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
